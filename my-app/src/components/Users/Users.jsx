c894ebd6e098e26469056abeed92b63440970a19
tinana2001 <tinana2013@gmail.com>
2024-05-28 19:50:18 +0500

    Создана классовая компонента Users

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -2,41 +2,48 @@ import React from 'react';
 import  axios from 'axios';
 import styles from './Users.module.css'
 import userPhoto from '../../assets/imeges/cat-siluet.png'
-const Users = (props) => {
-	if (props.users.length === 0){
-		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
-		props.setUsers(response.data.items);
-		});
+
+class Users extends React.Component{
+
+	getUsers=()=> {
+		if (this.props.users.length === 0){
+			axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
+			this.props.setUsers(response.data.items);
+			});
+		} 
 	}
-	return (
-		<div>
-			{
-				props.users.map(u => <div key={u.id}>
-					<div className={styles.usersBlock}>
-						<div>
-							<div className={styles.photo}>
-								<img src={u.photos.small !=null ? u.pgotos.small :userPhoto } />
-							</div>
+	render(){
+		return (
+			<div>
+				<button className={styles.btnUsers} onClick={this.getUsers}>Get Users</button>
+				{
+					this.props.users.map(u => <div key={u.id}>
+						<div className={styles.usersBlock}>
 							<div>
-								{u.followed ? 
-								<button onClick={()=>{props.unfollow(u.id)}} className={styles.but}>Unfollow</button> 
-								: <button onClick={()=>{props.follow(u.id)}} className={styles.but}>Follow</button>}
+								<div className={styles.photo}>
+									<img src={u.photos.small !=null ? u.pgotos.small :userPhoto } />
 								</div>
-						</div>
-						<div className={styles.nameAndLocation}>
-							<div>
-								<div className={styles.fullName}>{u.name}</div>
-								<div className={styles.status}>{u.status}</div>
+								<div>
+									{u.followed ? 
+									<button onClick={()=>{this.props.unfollow(u.id)}} className={styles.but}>Unfollow</button> 
+									: <button onClick={()=>{this.props.follow(u.id)}} className={styles.but}>Follow</button>}
+									</div>
 							</div>
-							<div className={styles.location}>
-								<div>{'u.location.country'}</div>
-								<div>{'u.location.city'}</div>
+							<div className={styles.nameAndLocation}>
+								<div>
+									<div className={styles.fullName}>{u.name}</div>
+									<div className={styles.status}>{u.status}</div>
+								</div>
+								<div className={styles.location}>
+									<div>{'u.location.country'}</div>
+									<div>{'u.location.city'}</div>
+								</div>
 							</div>
 						</div>
-					</div>
-				</div>)
-			}
-		</div>
-	)
+					</div>)
+				}
+			</div>
+		)
+	}
 }
 export default Users;
